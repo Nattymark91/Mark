a9e84b7df9fd88ecbf4bb07ebe52de1736c81315
Nattymark91 <157714294+Nattymark91@users.noreply.github.com>
2024-01-30 15:17:45 +0200

    Update jun_table.js

--- src/js/jun_table.js
+++ src/js/jun_table.js
@@ -1,6 +1,6 @@
 ////////ССЫЛКИ ДЛЯ ЗАГРУЗКИ//////////////////////////////////////
-const urlSmall = "http://www.filltext.com/?rows=32&id=%7Bnumber%7C1000%7D&firstName=%7BfirstName%7D&lastName=%7BlastName%7D&email=%7Bemail%7D&phone=%7Bphone%7C(xxx)xxx-xx-xx%7D&address=%7BaddressObject%7D&description=%7Blorem%7C32%7D";
-const urlBig = "http://www.filltext.com/?rows=1000&id=%7Bnumber%7C1000%7D&firstName=%7BfirstName%7D&delay=3&lastName=%7BlastName%7D&email=%7Bemail%7D&phone=%7Bphone%7C(xxx)xxx-xx-xx%7D&address=%7BaddressObject%7D&description=%7Blorem%7C32%7D";
+const urlSmall = "https://www.filltext.com/?rows=32&id=%7Bnumber%7C1000%7D&firstName=%7BfirstName%7D&lastName=%7BlastName%7D&email=%7Bemail%7D&phone=%7Bphone%7C(xxx)xxx-xx-xx%7D&address=%7BaddressObject%7D&description=%7Blorem%7C32%7D";
+const urlBig = "https://www.filltext.com/?rows=1000&id=%7Bnumber%7C1000%7D&firstName=%7BfirstName%7D&delay=3&lastName=%7BlastName%7D&email=%7Bemail%7D&phone=%7Bphone%7C(xxx)xxx-xx-xx%7D&address=%7BaddressObject%7D&description=%7Blorem%7C32%7D";
 
 ////////ЭЛЕМЕНТЫ РАБОЧЕЙ ТАБЛИЦЫ//////////////////////////////////////
 const table = document.querySelector(".table-sortable");
